8385520a69b6b3259c340a242917408b86574cd5
chillingBaconhair <prem.sonic2011@gmail.com>
2025-08-30 15:30:12 +0530

    Initial commit

---
+++ src/main/java/net/ggdeeznuts/firstandlast/FirstAndLastClient.java
@@ -0,0 +1,10 @@
+package net.ggdeeznuts.firstandlast;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstAndLastClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/ggdeeznuts/firstandlast/FirstAndLastDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ggdeeznuts.firstandlast;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FirstAndLastDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
